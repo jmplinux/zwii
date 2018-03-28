014ad3f5584451842bece88df64a81dcae25580b
jmplinux <37860004+jmplinux@users.noreply.github.com>
2018-03-28 07:59:19 +0200

    Add files via upload

---
+++ core/module/user/view/index/index.js.php
@@ -0,0 +1,18 @@
+/**
+ * This file is part of Zwii.
+ *
+ * For full copyright and license information, please see the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Rémi Jean <remi.jean@outlook.com>
+ * @copyright Copyright (C) 2008-2018, Rémi Jean
+ * @license GNU General Public License, version 3
+ * @link http://zwiicms.com/
+ */
+
+/**
+ * Confirmation de suppression
+ */
+$(".userDelete").on("click", function() {
+	return confirm("Êtes-vous sûr de vouloir supprimer cet utilisateur ?");
+});
